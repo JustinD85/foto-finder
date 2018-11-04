8c0ab7d4de628951f022ed00c033c9b1e223b94c
Justin <justin@baal-software.com>
2018-11-03 21:36:15 -0600

    Favorite button is enabled

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 const uploadLabel = get('#file-selector-label');
 const fileInput = get('#img-upload');
-const imgsOnDomElem = get('#card-area');
+const cardArea = get('#card-area');
 const addToCardArea = get('#add');
 const reader = new FileReader();
+const favButton = get('.card-fav');
+const viewFavButton = get('#fav-button');
 
 const images = (() => {
   const imagesArray = [];
-  let tempImg = 1;
-  let nextId = 0;
+  let tempImg = 0;
+  let snextId = 0;
   return () => {
     return {
       add: (inUrl) => {
         const lastElementIndex = imagesArray.length - 1;
-        if (imagesArray.length === 0) {
-          nextId = 0;
-        } else {
+        if (imagesArray.length !== 0) {
           nextId = imagesArray[lastElementIndex].id + 1;
         }
         const title = get('#title-input').value;
@@ -22,9 +22,17 @@ const images = (() => {
         const newImg = new Photo(nextId, title, caption, inUrl, false);
         tempImg = newImg;
       },
-      remove: 'remove from local storage and datamodel',
-      update: 'update local storage and datamodel',
-      get: () => (imagesArray),
+      remove: (inId) => {
+        console.log(inId)
+        const tempIndex = imagesArray.findIndex(obj => obj.id === inId);
+        imagesArray.splice(tempIndex, 1);
+        imagesArray[tempIndex].deleteFromStorage(imagesArray);
+      },
+      update: (inId) => {
+        const tempIndex = imagesArray.findIndex(obj => obj.id === parseInt(inId));
+        // imagesArray[tempIndex].
+      },
+      asArray: () => (imagesArray),
       publish: () => {
         console.log('Publishing!');
         get('#card-area').innerHTML = '';
@@ -41,30 +49,84 @@ const images = (() => {
 })();
 
 window.onload = () => {
+  let favs;
   if (localStorage.getItem('imgs') !== null) {
     let tempImgsArr = JSON.parse(localStorage.getItem('imgs'));
     tempImgsArr.forEach(ele => {
       let tempCard = new Photo(ele.id, ele.title, ele.caption, ele.file, ele.favorite);
       addToDOM(tempCard);
-      images().get().push(tempCard);
-      console.log('loaded', tempImgsArr);
+      images().asArray().push(tempCard);
     });
+    console.log('# objs loaded', tempImgsArr);
   }
+  favs = images().asArray().filter(e => e.favorite === true).length;
+  viewFavButton.innerText = `View ${favs} Favorites`;
 }
+
 uploadLabel.addEventListener('click', e => {
   e.preventDefault();
   fileInput.click();
-})
+});
+
 addToCardArea.addEventListener('click', e => {
   e.preventDefault();
   images().publish();
-})
+});
 
 fileInput.addEventListener('change', () => {
   upload(fileInput.files);
+});
+
+reader.addEventListener('load', () => images().add(reader.result));
+
+cardArea.addEventListener('click', (e) => {
+  //Favorite behaviour
+  if (e.target.closest('.card-fav')) {
+    const inId = e.target.closest('.card').dataset.id;
+    const atThisIndex = images().asArray().findIndex(e => e.id == inId);
+    const cardTitle = get(`.card[data-id='${inId}'] .card-title`).innerText;
+    const cardDesc = get(`.card[data-id='${inId}'] .card-desc`).innerText;
+    const cardFile = get(`.card[data-id='${inId}'] .card-img`).src;
+    const isFav = get(`.card[data-id='${inId}'] .card-fav`).attributes.src.value !== 'imgs/favorite-active.svg';
+    let favs;
+    e.target.src = e.target.attributes.src.value == 'imgs/favorite-active.svg' ?
+      'imgs/favorite.svg' : 'imgs/favorite-active.svg';
+
+    //this will be update self function
+    images().asArray()[atThisIndex] = new Photo(inId, cardTitle, cardDesc, cardFile, isFav);
+    favs = images().asArray().filter(e => e.favorite === true).length;
+    viewFavButton.innerText = `View ${favs} Favorites`;
+  }
+  if (e.target.closest('.card-trash')) {
+    e.target.src = e.target.attributes.src.value == 'imgs/delete-active.svg' ?
+      'imgs/delete.svg' : 'imgs/delete-active.svg';
+  }
 })
 
-reader.addEventListener('load', () => images().add(reader.result))
+//Delete Behaviour
+cardArea.addEventListener('mousedown', (e) => {
+  if (e.target.closest('.card-trash')) {
+    e.target.src = e.target.attributes.src.value == 'imgs/delete-active.svg' ?
+      'imgs/delete.svg' : 'imgs/delete-active.svg';
+  }
+});
+
+//If you press delete, but don't release, reset svg
+cardArea.addEventListener('mouseout', (clicked) => {
+  if (clicked.target.closest('.card-trash')) {
+    clicked.target.src = 'imgs/delete.svg';
+  }
+});
+
+//The actual delete, if a legal press
+cardArea.addEventListener('mouseup', (e) => {
+  if (e.target.closest('.card-trash') &&
+    e.target.attributes.src.value === 'imgs/delete-active.svg') {
+    images().remove(e.target.closest('.card').dataset.id);
+    e.target.closest('.card').remove();
+  }
+});
+
 
 function upload(files) {
   console.log('upload called');
@@ -72,10 +134,9 @@ function upload(files) {
 }
 
 
-
 function addToDOM(img) {
   const newIdea = document.createElement('section');
-
+  const tempFav = img.favorite ? 'favorite-active.svg' : 'favorite.svg';
   get('#title-input').value = '';
   get("#caption-input").value = '';
   newIdea.classList.add('card');
@@ -87,7 +148,7 @@ function addToDOM(img) {
   <p class="card-desc">${img.caption}</p>
   <footer>
     <img class="card-trash" src="imgs/delete.svg" alt="Trash, to delete photo">
-    <img class="card-fav" src="imgs/favorite.svg" alt="A button to like the photo">
+    <img class="card-fav" src="imgs/${tempFav}" alt="A button to like the photo">
   </footer>`;
   get('#card-area').prepend(newIdea);
 }
